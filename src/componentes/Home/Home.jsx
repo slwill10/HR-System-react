f41d9d4455a7d40d712eecdd62676cb9a94ca6c2
Willian Santos <wsantosdelima44@gmail.com>
2024-06-21 22:28:54 -0300

    Puxando os dados do backend

--- src/componentes/Home/Home.jsx
+++ src/componentes/Home/Home.jsx
@@ -1,46 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import '../../index.css';
 import wolman from '../../assets/mulher mexendo no celular fundo verde.png';
+import api from '../../services/api';
 
 function Home() {
-    const vagas = [{
-        id: '21312321',
-        nome: 'Desenvolvedor java',
-        descricao: 'Estamos buscando um(a) Desenvolvedor(a) Junior Java para se juntar à nossa equipe dinâmica e colaborativa.' +
-            'Essa é uma ótima oportunidade para você iniciar sua carreira em desenvolvimento de software e aprender com profissionais experientes.',
-        data: '20-02-12',
-        salario: 4200
-    },
-    {
-        id: '2131eqweqwe2321',
-        nome: 'desenvolvedor full-stack',
-        descricao: 'Vaga de desenvolvedor sênior',
-        data: '02-01-21',
-        salario: 10000
-    },
-    {
-        id: '2131eqweqdads222we2321',
-        nome: 'desenvolvedor full-stack',
-        descricao: 'Vaga de desenvolvedor sênior',
-        data: '02-01-21',
-        salario: 10000
-    },
-    {
-        id: '2131eqwedadasdsads111111aqwe2321',
-        nome: 'desenvolvedor full-stack',
-        descricao: 'Vaga de desenvolvedor sênior',
-        data: '02-01-21',
-        salario: 10000
-    },
-    {
-        id: '2131eqweqwedasdsad4444a2321',
-        nome: 'desenvolvedor full-stack',
-        descricao: 'Vaga de desenvolvedor sênior',
-        data: '02-01-21',
-        salario: 10000
-    }];
-
+    const [vagas, setVagas] = useState([]);
     const [selectedVaga, setSelectedVaga] = useState(null);
+    const [filtroCidade, setFiltroCidade] = useState('');
+    const [filtroCargo, setFiltroCargo] = useState('');
+    const [vagasFiltradas, setVagasFiltradas] = useState([]);
+
+    useEffect(() => {
+        const getVagas = async () => {
+            try {
+                const response = await api.get('/vagas');
+                setVagas(response.data);
+                setVagasFiltradas(response.data); 
+            } catch (error) {
+                console.error('There was an error making the request:', error);
+            }
+        };
+        getVagas();
+    }, []);
+
+    useEffect(() => {
+        filtrarVagas(); 
+    }, [filtroCidade, filtroCargo, vagas]);
+
+    const filtrarVagas = () => {
+        const filteredVagas = vagas.filter(vaga => 
+            (filtroCidade === '' || vaga.localidade.toLowerCase().includes(filtroCidade.toLowerCase())) &&
+            (filtroCargo === '' || vaga.nome.toLowerCase().includes(filtroCargo.toLowerCase()))
+        );
+        setVagasFiltradas(filteredVagas);
+    };
 
     return (
         <div>
@@ -57,20 +50,35 @@ function Home() {
                     <hr className="border-solid border-t-1 border-black border-opacity-10 w-10/12 mx-auto md:w-5/12" />
                 </div>
                 <div className="flex flex-col items-center justify-center mt-8 md:flex-row md:mt-32">
-                    <input type="text" className="border border-gray-400 px-2 py-1 rounded-lg h-14 w-full md:w-96" placeholder="Digite um estado ou cidade" />
+                    <input 
+                        type="text" 
+                        className="border border-gray-400 px-2 py-1 rounded-lg h-14 w-full md:w-96" 
+                        placeholder="Digite um estado ou cidade"
+                        value={filtroCidade}
+                        onChange={(e) => setFiltroCidade(e.target.value)}
+                    />
                     <div className="h-12 border border-black hidden md:block"></div>
-                    <div class="relative flex items-center w-full md:w-auto">
-                        <input type="text" class="border border-gray-400 px-2 py-1 rounded-lg h-14 w-full md:w-96 pr-32" placeholder="Digite o cargo" />
-                        <button class="absolute right-2 bg-blue-500 text-white px-4 py-1 rounded-3xl h-12 border">Achar vaga</button>
+                    <div className="relative flex items-center w-full md:w-auto">
+                        <input 
+                            type="text" 
+                            className="border border-gray-400 px-2 py-1 rounded-lg h-14 w-full md:w-96 pr-32" 
+                            placeholder="Digite o cargo"
+                            value={filtroCargo}
+                            onChange={(e) => setFiltroCargo(e.target.value)}
+                        />
+                        <button 
+                            className="absolute right-2 bg-blue-500 text-white px-4 py-1 rounded-3xl h-12 border"
+                            onClick={filtrarVagas}
+                        >
+                            Achar vaga
+                        </button>
                     </div>
                 </div>
-
             </div>
-
             <div className="pt-96 flex">
                 <div className="w-1/2 h-[500px] overflow-y-auto">
-                    {vagas.map(vaga => (
-                        <div key={vaga.id}
+                    {vagasFiltradas.map(vaga => (
+                        <div key={vaga.codigo} 
                             className="flex justify-between bg-[#D9D9D9] m-2.5 p-2.5 rounded-xl cursor-pointer"
                             onClick={() => setSelectedVaga(vaga)}>
                             <div>
@@ -78,18 +86,20 @@ function Home() {
                                 <p><strong>Descrição:</strong> <span>{vaga.descricao}</span></p>
                                 <p><strong>Data:</strong> <span>{vaga.data}</span></p>
                                 <p><strong>Salário:</strong> <span>{vaga.salario}</span></p>
+                                <p><strong>Localidade:</strong> {vaga.localidade}</p>
                             </div>
                         </div>
                     ))}
                 </div>
                 <div className="w-1/2 flex items-center justify-center">
-                    <div className="bg-[#D9D9D9] p-4 rounded-xl shadow-lg max-w-[900px] w-full h-[500px] overflow-y-auto mt-3">
+                    <div className="bg-[#D9D9D9] p-4 rounded-xl shadow-lg max-w-[500px] w-full h-[500px] overflow-y-auto mt-3">
                         {selectedVaga ? (
                             <div>
                                 <h2 className="text-2xl font-bold mb-2">{selectedVaga.nome}</h2>
                                 <p><strong>Descrição:</strong> {selectedVaga.descricao}</p>
                                 <p><strong>Data:</strong> {selectedVaga.data}</p>
                                 <p><strong>Salário:</strong> {selectedVaga.salario}</p>
+                                <p><strong>Localidade:</strong> {selectedVaga.localidade}</p>
                             </div>
                         ) : (
                             <h1 className="text-xl">Selecione uma vaga para ver os detalhes</h1>
